05c23e0463de166379c171a9eed467f858bc60ef
julio <anezjulio>
2023-07-25 06:28:15 -0300

    delete endpoint

--- src/main/java/com/tilin/core/product/Application.java
+++ src/main/java/com/tilin/core/product/Application.java
@@ -2,12 +2,25 @@ package com.tilin.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/core-product/v1/list-product").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 
 }
--- src/main/java/com/tilin/core/product/insfrastructure/controller/ProductController.java
+++ src/main/java/com/tilin/core/product/insfrastructure/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.tilin.core.product.insfrastructure.controller;
 
+import com.tilin.core.product.application.dto.DeleteProductResponseDTO;
 import com.tilin.core.product.application.dto.RequestProductDTO;
 import com.tilin.core.product.domain.models.Product;
 import com.tilin.core.product.domain.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,33 +17,52 @@ import java.util.Optional;
 @RequestMapping("/core-product/v1")
 public class ProductController {
 
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping(value="/list-product")
-    public ResponseEntity<List<Product>> listProduct(){
-        return ResponseEntity.ok(productRepository.findAll());
+    @GetMapping(value = "/list-product")
+    public ResponseEntity<List<Product>> listProduct() {
+        List<Product> productList = productRepository.findAll();
+        logger.info("---------------------------------------------");
+        productList.stream().forEach(
+                product -> logger.info(product.getName())
+        );
+        return ResponseEntity.ok(productList);
     }
 
-    @GetMapping(value="/find-product/{productId}")
+    @GetMapping(value = "/find-product/{productId}")
     public ResponseEntity<Optional<Product>> getProduct(
             @PathVariable Long productId
-    ){
+    ) {
         return ResponseEntity.ok(productRepository.findById(productId));
     }
 
-    @PostMapping(value ="/insert-product" )
+    @PostMapping(value = "/insert-product")
     public ResponseEntity<List<Product>> insertProduct(
             @RequestBody
             RequestProductDTO requestProductDTO
-    ){
+    ) {
         List<Product> productList = requestProductDTO.getProductList();
-       // for (Product product : productList) {
-       //     productRepository.save(product);
-        //}
         productRepository.saveAll(productList);
         return ResponseEntity.ok(productList);
 
+    }
+
+    @PostMapping(value = "/modify-product")
+    public ResponseEntity<List<Product>> modifyProduct(
+            @RequestBody  RequestProductDTO requestProductDTO
+    ) {
+        return ResponseEntity.ok(productRepository.saveAllAndFlush(requestProductDTO.getProductList()));
+    }
+
+    @GetMapping(value = "/delete-product/{productId}")
+    public ResponseEntity<DeleteProductResponseDTO>deleteProduct(
+            @PathVariable Long productId
+    ) {
+        productRepository.deleteById(productId);
+        return ResponseEntity.ok(new DeleteProductResponseDTO("id: " + productId + " has been deleted."));
     }
 
 }
